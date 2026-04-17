e905967d47bb1e2bd31dd4a58bfa738f8ddead23
Nocquet-Gabriel <gabriel.nocquet@outlook.com>
2026-04-17 17:22:06 +0200

    generateBatailleNavale fonctionnelle

--- script.js
+++ script.js
@@ -41,21 +41,46 @@ function guessthenumberStart(){
 
 }
 function caseClicked(caseNumber){
-    console.log("Case cliquée"+ caseNumber);
+    console.log("Case cliquée "+ caseNumber);
+}
+
+function caseClickedBatailleNavale(caseNum){
+    alert(caseNum);
 }
 
 
 function generateBatailleNavale(){
     let table = document.getElementById("tableau");
     lettrelist = ["A","B","C","D","E","F","G","H","I","J"];
-    for (let i = 0; i<10; i++){
+    let inittr = document.createElement("tr");
+    for(let i=0; i<=10; i++){
+        if (i==0){
+            let initth = document.createElement("th");
+            inittr.appendChild(initth);
+        } else {
+        let initth = document.createElement("th");
+        initth.textContent = lettrelist[i-1];
+        inittr.appendChild(initth);
+        }
+    }
+    table.appendChild(inittr);
+    for (let i = 1; i<=10; i++){
         let tr = document.createElement("tr");
         let th = document.createElement("th");
         th.textContent = i;
-        for (let j=0; j<=10; j++){
-            if(j==0){
-                td= document.createElement("td")
-            }
+        tr.appendChild(th);
+        for (let j=0; j<10; j++){
+            td = document.createElement("td");
+                td.addEventListener("click", () =>{
+                    caseClickedBatailleNavale(lettrelist[j]+i);
+                })
+            
+            tr.appendChild(td);
         }
+        table.appendChild(tr);
+        
     }
+    btnjouer.style.display = 'none';
+    titre.style.display = 'none';
+    table.style.display = 'initial';
 }
